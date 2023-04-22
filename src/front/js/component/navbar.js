0a2869d7802ac0379ff262ed97d3d2a69de998f8
zaimonsays <simon.ramirez.ojeda@gmail.com>
2023-04-21 20:11:44 -0400

    nuevo commit

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,15 +3,37 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<nav id="navbar" className="navbar navbar-expand-lg bg-body-tertiary mb-5">
+			<div className="container-fluid">
+				<a className="navbar-brand text-white navbar-brand m-3 h1 ps-4" href="#">
+					TAK LOGO
+				</a>
+				<div className="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul className="navbar-nav ms-auto ps-5">
+						<li className="nav-item pe-4">
+							<a className="nav-link active text-white" aria-current="page" href="#">
+								TAK
+							</a>
+						</li>
+						<li className="nav-item pe-4">
+							<a className="nav-link active text-white" aria-current="page" href="#">
+								RESPIRA
+							</a>
+						</li>
+						<li className="nav-item pe-4">
+							<a className="nav-link active text-white" aria-current="page" href="#">
+								DESAFIOS
+							</a>
+						</li>
+						<li className="nav-item pe-4">
+							<a className="nav-link active text-white" aria-current="page" href="#">
+								DONA AQUI
+							</a>
+						</li>
+						<li className="nav-item pe-4">
+							<button type="button" class="btn btn-outline-light">INGRESA</button>
+						</li>
+					</ul>
 				</div>
 			</div>
 		</nav>
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { UserPerfil } from "./pages/perfiluser";
 
 //create your first component
 const Layout = () => {
@@ -29,6 +30,7 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
+                        <Route element={<UserPerfil />} path="/user/:thid" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
