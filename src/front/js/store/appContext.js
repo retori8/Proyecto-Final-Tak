10f7c7dda2454477d18ddffba786b7c2fb48e991
carorb <reciclee@gmail.com>
2023-05-15 23:24:05 -0400

    caro

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -24,7 +24,7 @@ const injectContext = PassedComponent => {
 		useEffect(() => {
 
 			state.actions.getBooks();
-			state.actions.getAudios();
+			// state.actions.getAudios();
 			state.actions.getFavoritos();
 			state.actions.getArchivos();
 			state.actions.getDays()
